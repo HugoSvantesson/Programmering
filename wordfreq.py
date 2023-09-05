f76dfc6d6563a068b3d804c6679bc815ebdaf38f
Batroman <144106508+Batroman@users.noreply.github.com>
2023-09-05 12:30:20 +0200

    Add files via upload

--- wordfreq.py
+++ wordfreq.py
@@ -1,2 +1,17 @@
-def tokenize():
-  
+
+# document = ['"Come on you spurs"']
+document = "Come on you, Spurs"
+def tokenize(words):
+   list = []
+   new_document = words.split()
+  # for line in words:
+   #  words.split()
+   for x in new_document:
+      list.append(x)
+   
+   return list
+
+print(tokenize(document))
+
+
+
