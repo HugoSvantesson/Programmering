3cdee2c1a958863c3ce08d88f5c42d1d78bb59eb
Anton02notna <144107374+Anton02notna@users.noreply.github.com>
2023-09-06 16:44:38 +0200

    Update wordfreq.py

--- wordfreq.py
+++ wordfreq.py
@@ -1,12 +1,11 @@
-
 def tokenize(lines):
     words = []
     for line in lines:
-         start = 0
-         while start < len(line):
+        start = 0
+        while start < len(line):
+            while start < len(line) and line[start].isspace():
+                start = start + 1
+
             print(line[start])
-            start = start+1
-
-print(tokenize("I am, cool!"))
-
-
+            start = start + 1
+    return words
